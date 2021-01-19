44284bd1ab62dfc07d07e7589b350d2206b02e2c
akaver <akaver@akaver.com>
2021-01-19 09:58:37 +0200

    initial

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using VatServiceReference;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index(string state = "EE" )
+        {
+            var vatClient = new vatRetrievalServiceClient();
+            var reqParams = new retrieveVatRatesReqType();
+            reqParams.memberStates = new[] {state};
+            reqParams.situationOn = DateTime.Now;
+
+            var response = await vatClient.retrieveVatRatesAsync(reqParams);
+
+
+            return View(response);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
